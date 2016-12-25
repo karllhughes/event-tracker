7315001eb2d14821591517e47e761294076d067f
Karl Hughes <khughes.me@gmail.com>
2016-12-25 17:02:37 -0600

    improving more view components

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,15 @@
 #
 
 # Create 25 events
+Event.delete_all
 25.times do |i|
   Event.create(
-      title: Faker::Name.unique.name,
+      title: Faker::Lorem.sentence,
       description: Faker::Lorem.paragraph,
-      location: Faker::Lorem.word,
+      location: Faker::Lorem.word + ', ' + Faker::Lorem.word,
       cost: Faker::Number.decimal(2),
       url: Faker::Internet.url,
-      image: Faker::LoremPixel.image,
+      image: Faker::LoremPixel.image('300x150'),
       registration_starts_at: Faker::Date.forward(0),
       registration_ends_at: Faker::Date.forward(0),
       starts_at: Faker::Date.forward(0),
