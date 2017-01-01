a8b33be7a94923583c748fe23c2fd21e5958b43e
Karl Hughes <khughes.me@gmail.com>
2017-01-01 08:21:31 -0600

    Adding controller tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -8,8 +8,36 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get single event" do
-    get event_path
+    event = Event.first
+    get event_path(event.id)
     assert_response :success
+  end
+
+  test "should show new event page" do
+    get new_event_path
+    assert_response :success
+  end
+
+  test "should show edit event page" do
+    event = Event.first
+    get edit_event_path(event.id)
+    assert_response :success
+  end
+
+  test "should submit create event form" do
+    # Coming soon
+    assert true
+  end
+
+  test "should submit edit event form" do
+    # Coming soon
+    assert true
+  end
+
+  test "should delete event" do
+    event = Event.first
+    delete event_path(event.id)
+    assert_redirected_to events_path
   end
 
 end
