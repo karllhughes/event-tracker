c64ce8dd19ac8f3819d33c658d212a53a0fa3451
Karl Hughes <khughes.me@gmail.com>
2017-01-01 08:05:48 -0600

    Adding model tests

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save event without valid title and url" do
+    event = Event.new
+    assert_not event.save
+  end
+
+  test "should save event with valid title and url" do
+    event = Event.new(title: Faker::Lorem.sentence, url: Faker::Internet.url)
+    assert event.save
+  end
 end
