6e6b620a84d3465ec9f22f373182e3268ebd77a7
Karl Hughes <khughes.me@gmail.com>
2017-01-01 08:07:41 -0600

    Adding model tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
+
   test "should get index" do
     get events_path
+    assert_response :success
+  end
+
+  test "should get single event" do
+    get event_path
     assert_response :success
   end
 
